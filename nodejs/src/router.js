047013737ba6fff41d993e04194b9d35e3ebf210
xo-0xx <143212424+xo-0xx@users.noreply.github.com>
2024-03-26 23:16:13 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -2,11 +2,10 @@ import kunyu77 from './spider/video/kunyu77.js';
 import kkys from './spider/video/kkys.js';
 import push from './spider/video/push.js';
 import alist from './spider/pan/alist.js';
-import _13bqg from './spider/book/13bqg.js';
 import copymanga from './spider/book/copymanga.js';
 import ffm3u8 from './spider/video/ffm3u8.js';
 
-const spiders = [kunyu77, kkys, ffm3u8, push, alist, _13bqg, copymanga];
+const spiders = [kunyu77, kkys, ffm3u8, push, alist, copymanga];
 const spiderPrefix = '/spider';
 
 /**
